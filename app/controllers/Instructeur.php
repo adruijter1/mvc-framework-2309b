0c633484665e2955a3488bee6ed97c9d59c59686
Arjan de Ruijter <rra@mboutrecht.nl>
2024-02-05 13:37:33 +0100

    Core class is af

---
+++ app/controllers/Instructeur.php
@@ -0,0 +1,15 @@
+<?php
+
+class Instructeur
+{
+
+    public function __construct()
+    {
+        echo "test";
+    }
+
+    public function index()
+    {
+        echo "Dit is de index method";
+    }
+}
