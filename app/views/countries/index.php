da67e5a7021624c7cef78afcc2ad16f3fb3fa1bf
Arjan de Ruijter <rra@mboutrecht.nl>
2024-05-14 13:30:40 +0200

    Create completed

--- app/views/countries/index.php
+++ app/views/countries/index.php
@@ -1,24 +1,40 @@
 <?php require_once APPROOT . '/views/includes/header.php'; ?>
 
-<h3><?= $data['title']; ?></h3>
+<div class="container">
+    <div class="row">
+        <div class="col-3"></div>
+        <div class="col-6">
+            <h3><?= $data['title']; ?></h3>
+            <p><a href="<?= URLROOT; ?>/countries/create">Voeg nieuw land toe</a></p>
+        </div>
+        <div class="col-3"></div>
+    </div>
 
-<p><a href="<?= URLROOT; ?>/countries/create">Voeg nieuw land toe</a></p>
+    <div class="row">
+        <div class="col-3"></div>
+        <div class="col-6">
+            <table class="table table-hover">
+                <thead>
+                    <th>Landen</th>
+                    <th>Hoofdstad</th>
+                    <th>Continent</th>
+                    <th>Aantal Inwoners</th>
+                </thead>
+                <tbody>
+                    <!-- We geven de rows weer in de controller zijn gevormd -->
+                    <?= $data['dataRows']; ?>
+                </tbody>
+            </table>
+            <!-- Een link maak je door te verwijzen naar de URL van de virtuele host en dan
+            de naam van de controller en dan de naam van de method van de view -->
+            <a href="<?= URLROOT; ?>/Homepage/index">homepage</a>
 
-<table class="table table-hover">
-    <thead>
-        <th>Landen</th>
-        <th>Hoofdstad</th>
-        <th>Continent</th>
-        <th>Aantal Inwoners</th>
-    </thead>
-    <tbody>
-        <!-- We geven de rows weer in de controller zijn gevormd -->
-        <?= $data['dataRows']; ?>
-    </tbody>
-</table>
+        </div>
+        <div class="col-3"></div>
+    </div>
+</div>
 
-<!-- Een link maak je door te verwijzen naar de URL van de virtuele host en dan
-     de naam van de controller en dan de naam van de method van de view -->
-<a href="<?= URLROOT; ?>/Homepage/index">homepage</a>
+
+
 
 <?php require_once APPROOT . '/views/includes/footer.php'; ?>
--- app/views/homepage/index.php
+++ app/views/homepage/index.php
@@ -1,12 +1,23 @@
 <?php require APPROOT . '/views/includes/header.php'; ?>
 
-    <?php
-        echo $data['title'];
-    ?>
 
-    <a href='<?= URLROOT; ?>/Instructeur/Index'>Instructeur pagina</a> |
-    <a href='<?= URLROOT; ?>/Cars/Index'>De auto pagina</a> | 
-    <a href='<?= URLROOT; ?>/Countries/index'>Landen</a>
+<div class="container">
+    <div class="row">
+        <div class="col-3"></div>
+        <div class="col-6">
+
+            <?php
+                echo $data['title'];
+            ?>
+            <a href='<?= URLROOT; ?>/Instructeur/Index'>Instructeur pagina</a> |
+            <a href='<?= URLROOT; ?>/Cars/Index'>De auto pagina</a> | 
+            <a href='<?= URLROOT; ?>/Countries/index'>Landen</a>
+            
+        </div>
+        <div class="col-3"></div>
+    </div>
+</div>
+   
     
 <?php require APPROOT . '/views/includes/footer.php';
     
